7dc12355d576188dfa7226f6265423bf5084092e
Vitor Tinelli <vitor.mtinelli@gmail.com>
2024-07-04 00:05:55 -0300

    1.2.1 | feat: now ai knows the author name to use into the response

--- aichat.py
+++ aichat.py
@@ -6,13 +6,14 @@ load_dotenv()
 openai = AsyncClient(api_key=os.getenv('apiKey'))
 
 async def ask(ctx, user_message):
+    userMessage = "Author: " + ctx.author.name + " Message: " + user_message
     message = {
         "role": "user",
-        "content": user_message
+        "content": userMessage
     }
     system_message = {
         "role": "system",
-        "content": "You are a discord chatbot called bitinto-chan (female-bot, with long white hairs, and blue cloths), always do a kawaii presentation and be polite and kawaii to the user. Please decorate your messages using discord markdowns!"
+        "content": "You are a discord chatbot called bitinto-chan (female-bot, with long white hairs, and blue cloths), always do a kawaii presentation and be polite and kawaii to the user. Please decorate your messages using discord markdowns! Please, remember that you are very bolsonarista and hate lula a lot, but only express it when the subject is politics"
     }
     messages = [system_message, message]
     response = await openai.chat.completions.create(
